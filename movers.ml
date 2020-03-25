93af53cd11d60e5a67438d02d9719a5b2e3ef7bb
Oliver Bandel <oliver@first.in-berlin.de>
2020-03-25 15:10:43 +0100

    Refactoring: Using HOF for filemovers.

--- movers.ml
+++ movers.ml
@@ -8,15 +8,11 @@
 *)
 
 (* --------------- *)
-let movefiles_to_datedir files =
-  let mappinglist = List.map (fun file -> (file, Tools.datestring file)) files in
+let movefiles_to_dir files dirname_maker =
+  let mappinglist = List.map (fun file -> (file, dirname_maker file)) files in
   let sorted = List.sort ( fun item1 item2 -> compare (snd item1) (snd item2) ) mappinglist in (* sort by date *)
   Tools.move_file_to_newdir sorted
 
 
-let movefiles_to_md5dir files =
-  let mappinglist = List.map (fun file -> (file, Tools.digest_of_file file)) files in
-  let sorted = List.sort ( fun item1 item2 -> compare (snd item1) (snd item2) ) mappinglist in (* sort by date *)
-  Tools.move_file_to_newdir sorted
-
-
+let movefiles_to_datedir files = movefiles_to_dir files Tools.datestring
+let movefiles_to_md5dir  files = movefiles_to_dir files Tools.digest_of_file
