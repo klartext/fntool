6a5b5d9afd164268ab53ce3806298627ba4a6231
Oliver Bandel <oliver@first.in-berlin.de>
2024-05-04 02:12:48 +0200

    movers.ml: type declaration added for fileinfo param in anonyme func in List.map

--- movers.ml
+++ movers.ml
@@ -7,12 +7,10 @@
   Use this software at your own risk.
 *)
 
-open Fileinfo
-
 (* --------------- *)
 let movefiles_to_dir mappinglist =
   let sorted = List.sort ( fun item1 item2 -> compare (snd item1) (snd item2) ) mappinglist in (* sort by dirname *)
-  let fnsorted = List.map (fun (fileinfo, dir) -> fileinfo.fni.filename, dir) sorted in
+  let fnsorted = List.map (fun ((fileinfo : Fileinfo.fileinfo), dir) -> fileinfo.fni.filename, dir) sorted in
   Tools.move_files_to_newdir fnsorted
 
 
