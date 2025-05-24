d8188ef35d41e4ae4f0a76645260a53a6eaa969d
Oliver Bandel <oliver@first.in-berlin.de>
2025-05-24 17:34:47 +0200

    shortening linelength by using tmp vars

--- tools.ml
+++ tools.ml
@@ -22,18 +22,25 @@ let date_of_file  ?(month=false)
   let s = stat fname in
   let tm = s.st_mtime |>localtime in
 
+  let year = tm.tm_year in
+  let mon  = tm.tm_mon  in
+  let mday = tm.tm_mday in
+  let hr   = tm.tm_hour in
+  let min  = tm.tm_min  in
+  let sec  = tm.tm_sec  in
+
   if usefloat
   then
     Printf.sprintf "%f" s.st_mtime
   else
   begin (* no float *)
     match month, day, hours, minutes, seconds with
-      | false, false, false, false, false -> Printf.sprintf "%4d" (1900 + tm.tm_year)
-      | true,  false, false, false, false -> Printf.sprintf "%4d-%02d" (1900 + tm.tm_year) (1 + tm.tm_mon)
-      | _,     true,  false, false, false -> Printf.sprintf "%4d-%02d-%02d" (1900 + tm.tm_year) (1 + tm.tm_mon) (tm.tm_mday)
-      | _, _,         true,  false, false -> Printf.sprintf "%4d-%02d-%02d_%02dh" (1900 + tm.tm_year) (1 + tm.tm_mon) (tm.tm_mday) (tm.tm_hour)
-      | _, _, _,      true,  false -> Printf.sprintf "%4d-%02d-%02d_%02dh%02dm" (1900 + tm.tm_year) (1 + tm.tm_mon) (tm.tm_mday) (tm.tm_hour) (tm.tm_min)
-      | _, _, _,     _,     true  -> Printf.sprintf "%4d-%02d-%02d_%02dh%02dm%02ds" (1900 + tm.tm_year) (1 + tm.tm_mon) (tm.tm_mday) (tm.tm_hour) (tm.tm_min) (tm.tm_sec)
+      | false, false, false, false, false -> Printf.sprintf "%4d" (1900 + year)
+      | true,  false, false, false, false -> Printf.sprintf "%4d-%02d" (1900 + year) (1 + mon)
+      | _,     true,  false, false, false -> Printf.sprintf "%4d-%02d-%02d" (1900 + year) (1 + mon) mday
+      | _, _,         true,  false, false -> Printf.sprintf "%4d-%02d-%02d_%02dh" (1900 + year) (1 + mon) mday hr
+      | _, _, _,      true,  false -> Printf.sprintf "%4d-%02d-%02d_%02dh%02dm" (1900 + year) (1 + mon) mday hr min
+      | _, _, _,     _,     true  -> Printf.sprintf "%4d-%02d-%02d_%02dh%02dm%02ds" (1900 + year) (1 + mon) mday hr min sec
 
   end
 
